621764000ccd233b0bc2a2d623b9a96f5ec3e069
Gideon Low <hello@bygideon.com>
2023-05-07 14:52:40 +0800

    Added missing modals for customise-band.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
