05420092677e243e0e5f8efb26712a383f8cadac
Low Yiyiu <10480968+lowyiyiu@users.noreply.github.com>
2023-06-09 11:14:44 +0800

    Dev1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: "tw-",
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
